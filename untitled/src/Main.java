6990b20f0d9c1d2b6f41018364584f48d50e4949
ttstase <ttstase@gmail.com>
2023-04-02 16:47:21 +0200

    2commit

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Print myPrint = new Print();
+
+        myPrint.myPrint();
     }
 }
---
+++ untitled/src/Print.java
@@ -0,0 +1,7 @@
+public class Print {
+    public int myInt = 35;
+
+    public void myPrint(){
+        System.out.printf("\"---<%d>---\"%n", myInt);
+    }
+}
